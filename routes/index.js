17a74c62aec24ff7d592cb1893902e73c095fbe5
itsmraga-hub <ragawilliam570@gmail.com>
2023-10-04 11:39:54 +0300

    Task 5: First file

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,6 @@ import FilesController from '../controllers/FilesController';
 import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
 import { APIError, errorResponse } from '../middlewares/error';
 
-
 /**
  * Injects routes with their handlers to the given Express application.
  */
@@ -22,6 +21,14 @@ const injectRoutes = (api) => {
   // Authentication
   api.get('/connect', basicAuthenticate, AuthController.getConnect);
   api.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
+
+  // Files
+  api.post('/files', xTokenAuthenticate, FilesController.postUpload);
+  api.get('/files/:id', xTokenAuthenticate, FilesController.getShow);
+  api.get('/files', xTokenAuthenticate, FilesController.getIndex);
+  api.put('/files/:id/publish', xTokenAuthenticate, FilesController.putPublish);
+  api.put('/files/:id/unpublish', xTokenAuthenticate, FilesController.putUnpublish);
+  api.get('/files/:id/data', FilesController.getFile);
 
   api.all('*', (req, res, next) => {
     errorResponse(new APIError(404, `Cannot ${req.method} ${req.url}`), req, res, next);
