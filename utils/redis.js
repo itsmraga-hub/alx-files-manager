8faa8b333649fb2c8c81306293538e31e48a4944
itsmraga-hub <ragawilliam570@gmail.com>
2023-10-04 11:28:41 +0300

    Task 1: MongoDB utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,6 +1,7 @@
 import { promisify } from 'util';
 import { createClient } from 'redis';
 
+
 /**
  * Represents a Redis client.
  */
@@ -22,7 +23,6 @@ class RedisClient {
 
   /**
    * Checks if this client's connection to the Redis server is active.
-   * @returns {boolean}
    */
   isAlive() {
     return this.isClientConnected;
@@ -30,8 +30,6 @@ class RedisClient {
 
   /**
    * Retrieves the value of a given key.
-   * @param {String} key The key of the item to retrieve.
-   * @returns {String | Object}
    */
   async get(key) {
     return promisify(this.client.GET).bind(this.client)(key);
@@ -39,10 +37,6 @@ class RedisClient {
 
   /**
    * Stores a key and its value along with an expiration time.
-   * @param {String} key The key of the item to store.
-   * @param {String | Number | Boolean} value The item to store.
-   * @param {Number} duration The expiration time of the item in seconds.
-   * @returns {Promise<void>}
    */
   async set(key, value, duration) {
     await promisify(this.client.SETEX)
@@ -51,8 +45,6 @@ class RedisClient {
 
   /**
    * Removes the value of a given key.
-   * @param {String} key The key of the item to remove.
-   * @returns {Promise<void>}
    */
   async del(key) {
     await promisify(this.client.DEL).bind(this.client)(key);
